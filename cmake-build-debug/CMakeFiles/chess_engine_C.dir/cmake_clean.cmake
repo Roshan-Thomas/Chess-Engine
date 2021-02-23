60bd7d9d2cc286587be9c192f3cd48215960f1f6
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2021-02-23 16:40:10 +0300

    Board structure (definitions)

---
+++ cmake-build-debug/CMakeFiles/chess_engine_C.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chess_engine_C.dir/vice.c.obj"
+  "chess_engine_C.exe"
+  "chess_engine_C.exe.manifest"
+  "chess_engine_C.lib"
+  "chess_engine_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/chess_engine_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
