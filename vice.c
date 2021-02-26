915eaecb0157a137a98f9407121e1c359f9adf08
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2021-02-26 20:09:58 +0300

    Set and Clear bits

--- vice.c
+++ vice.c
@@ -4,19 +4,14 @@
 int main() {
 
     AllInit();
-
+    int index = 0;
     U64 playBitBoard = 0ULL;
 
-    playBitBoard |= (1ULL << SQ64(D2));
-    playBitBoard |= (1ULL << SQ64(D3));
-    playBitBoard |= (1ULL << SQ64(D4));
+    SETBIT(playBitBoard, 61); // Set a BIT (puts an X on the specified index)
+    PrintBitBoard(playBitBoard);
 
-    int sq64 = 0;
-    while (playBitBoard) {
-        sq64 = POP(&playBitBoard);
-        printf("popped: %d\n", sq64);
-        PrintBitBoard(playBitBoard);
-    }
+    CLRBIT(playBitBoard, 61); // Clears the BIT on the specified index
+    PrintBitBoard(playBitBoard);
 
     return 0;
 }
