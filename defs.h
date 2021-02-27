36dbba2ece02587240a3139176dc48d8c95b8253
Roshan Thomas <roshan.sunil.thomas@gmail.com>
2021-02-27 18:48:54 +0300

    adding Macros to Game Moves

--- defs.h
+++ defs.h
@@ -94,7 +94,27 @@ typedef struct {
 
 /* GAME MOVE */
 
+/*
+0000 0000 0000 0000 0000 0111 1111 -> From 0x3F
+0000 0000 0000 0011 1111 1000 0000 -> To >> 7, 0x3F
+0000 0000 0011 1100 0000 0000 0000 -> Captured >> 14, 0xF
+0000 0000 0100 0000 0000 0000 0000 -> EP 0x40000
+0000 0000 1000 0000 0000 0000 0000 -> Pawn Start 0x80000
+0000 1111 0000 0000 0000 0000 0000 -> Promoted Piece >> 20, 0xF
+0001 0000 0000 0000 0000 0000 0000 -> Castle 0x1000000
+*/
 
+#define FROM(m) ((m) & 0x3F)
+#define TO(m) (((m)>>7) & 0x3F)
+#define CAPTURED(m) (((m)>>14) & 0xF)
+#define PROMOTED(m) (((m)>>20) & 0xF)
+
+#define MFLAGEP 0x40000
+#define MFLAGPS 0x80000
+#define MFLAGCA 0x1000000
+
+#define MFLAGCAP 0x7C000
+#define MFLAGPROM 0xF00000
 
 /* MACROS */
 
